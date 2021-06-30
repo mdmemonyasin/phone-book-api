32c850fdf5868c0874603672669789346e1baf97
mdmemonyasin <memonyasin3@gmail.com>
2021-06-30 17:17:05 +0530

    done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
+console.log("hello");
 app.use(userRoutes);
 
 mongoose.connect('mongodb+srv://user1:Yasin@123@1-kthvn.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(result =>{
